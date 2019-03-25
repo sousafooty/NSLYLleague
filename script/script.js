752afd574089c44bf62ada7babe2c4479a2d55c5
jose sousa <jsousbr7@gmail.com>
2019-03-25 16:40:50 +0100

    jucing up

---
+++ script/script.js
@@ -0,0 +1,87 @@
+//Funciones para validar los campos del formulario.
+function validarNombre()
+{
+    var campoNombre = document.getElementById("first_name").value;
+
+    var campoValido = false;
+
+    if(String(campoNombre) == "" || String(campoNombre) == " ")
+    {
+        alert("Your name is empty");
+    }
+    else
+    {
+        campoValido = true;
+    }    
+
+    return campoValido;
+}
+
+function validarApellido()
+{
+    var campoApellido = document.getElementById("last_name").value;
+    var campoValido = false;
+
+    if(String(campoApellido) == "" || String(campoApellido) == " ")
+    {
+        alert("Your lastname is empty");
+    }
+    else
+    {
+        campoValido = true;
+    }
+
+    return campoValido;
+}
+
+function validarEmail()
+{
+    var email = document.getElementById("email").value;
+    var campoValido = false;
+
+    if(String(email) == "" || String(email) == " ")
+    {
+        alert("Your email is empty");
+    }
+    else
+    {
+        campoValido = true;
+    }
+
+    return campoValido;
+}
+
+function ValidarstreetAddress()
+{
+    var streetAddress = document.getElementById("street_address").value;
+    var campoValido = false;
+
+    if(String(streetAddress) == "" || String(streetAddress) == " ")
+    {
+        alert("Your street address is empty");
+    }
+    else
+    {
+        campoValido = true;
+    }
+
+    return campoValido;
+}
+
+function validarZipCode()
+{
+    var zip_code = document.getElementById("zip_code").value;
+    var campoValido = false;
+
+    if(String(zip_code) == "" || String(zip_code) == " ")
+    {
+        alert("Your zip code is empty");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+    return campoValido;
+}
